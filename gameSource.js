a9bdbb63d5fc561248cc7ee40e5566398ff76e59
Alexander Keefe <akeefe09@yahoo.com>
2018-05-23 19:11:55 -0400

    Restart project

--- gameSource.js
+++ gameSource.js
@@ -1,32 +1,31 @@
-const width = 500;
-const height = 500;
+
+// var ctx = screen.getContext('2d');
+//
+// var heliImage = new Image();
+// heliImage.src = "./assets/images/helicopter.png"
+//
+// var background = new Image();
+// background.src = "./assets/images/background.jpg"
+//
+// var CloudFunction = function() {
+//   var cloud = {
+//     X: Math.random() * width,
+//     Y: Math.random() * height,
+//     cloudImg: new Image(),
+//   };
+//   cloud.cloudImg.src = ("./assets/cloud" + Math.floor(Math.random()*2) + ".png")
+// }
+//
+//
+//
+// function draw() {
+//   ctx.drawImage(background, 0, 0);
+//   ctx.drawImage(pipeTop)
+//   ctx.drawImage(pipeBottom)
+//   ctx.drawImage(heliImage, 100, 100, 40, 40);
+//
+//   requestAnimationFrame(draw);
+// }
+
 
 var screen = document.getElementById("canvas");
-screen.width = width;
-screen.height = height;
-
-var ctx = screen.getContext('2d');
-
-var heliImage = new Image();
-heliImage.src = "./assets/images/helicopter.png"
-
-var background = new Image();
-background.src = "./assets/images/background.jpg"
-
-var CloudFunction = function() {
-  var cloud = {
-    X: Math.random() * width,
-    Y: Math.random() * height,
-    cloudImg: new Image(),
-  };
-  cloud.cloudImg.src = ("./assets/cloud" + Math.floor(Math.random()*2) + ".png")
-}
-
-
-
-function draw() {
-  ctx.drawImage(heliImage, 100, 100, 50, 50);
-  ctx.drawImage(background, 0, 0);
-
-  requestAnimationFrame(draw);
-}
