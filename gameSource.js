c2f416131217a8750cd886d364f62749e3876122
Alexander Keefe <akeefe09@yahoo.com>
2018-05-23 20:09:54 -0400

    Add some working visuals

--- gameSource.js
+++ gameSource.js
@@ -1,31 +1,45 @@
 
-// var ctx = screen.getContext('2d');
-//
-// var heliImage = new Image();
-// heliImage.src = "./assets/images/helicopter.png"
-//
-// var background = new Image();
-// background.src = "./assets/images/background.jpg"
-//
-// var CloudFunction = function() {
-//   var cloud = {
-//     X: Math.random() * width,
-//     Y: Math.random() * height,
-//     cloudImg: new Image(),
-//   };
-//   cloud.cloudImg.src = ("./assets/cloud" + Math.floor(Math.random()*2) + ".png")
-// }
-//
-//
-//
-// function draw() {
-//   ctx.drawImage(background, 0, 0);
-//   ctx.drawImage(pipeTop)
-//   ctx.drawImage(pipeBottom)
-//   ctx.drawImage(heliImage, 100, 100, 40, 40);
-//
-//   requestAnimationFrame(draw);
-// }
+window.onload = function() {
+  var screen = document.getElementById("canvas");
+  var ctx = screen.getContext("2d");
 
+  var heliImage = new Image();
+  heliImage.src = "images/helicopter.png";
 
-var screen = document.getElementById("canvas");
+  var background = new Image();
+  background.src = "images/background.jpg";
+
+  var pipeTop = new Image();
+  pipeTop.src = "images/pipetop.png";
+
+  var pipeBottom = new Image();
+  pipeBottom.src = "images/pipebottom.png";
+
+  var CloudFunction = function() {
+    var cloud = {
+      X: Math.random() * width,
+      Y: Math.random() * height,
+      cloudImg: new Image(),
+    };
+    cloud.cloudImg.src = ("images/cloud" + Math.floor(Math.random()*2) + ".png")
+  }
+
+  const space = 100;
+  let gap = 242 + space;
+
+  let heliX = 10;
+  let heliY = 200;
+  let fallRate = 3;
+
+  function draw() {
+    ctx.drawImage(background, 0, 0);
+    ctx.drawImage(pipeTop, 200, 0);
+    ctx.drawImage(pipeBottom, 200, 0+gap);
+    ctx.drawImage(heliImage, heliX, heliY);
+    heliY += fallRate;
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+
+}
